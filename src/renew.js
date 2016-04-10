6959224c159a5d2453c5e936c11d052c610bd115
rst8196 <rst8196@gmail.com>
2016-04-10 16:17:46 +0300

    Fixed () after ifs

--- src/renew.js
+++ src/renew.js
@@ -9,7 +9,7 @@ function findOldCreep(storage)
         var findOldCreepRet = nearbyCreeps[0];
     for (i=0; i < nearbyCreeps.length; i++)
     {
-        if nearbyCreeps[i].ticksToLive < findOldCreepRet.ticksToLive
+        if (nearbyCreeps[i].ticksToLive < findOldCreepRet.ticksToLive)
             findOldCreepRet = nearbyCreeps[i]
     }
     return findOldCreepRet
@@ -17,7 +17,7 @@ function findOldCreep(storage)
 
 function renewCreep(storage, creep)
 {
-    if creep.ticksToLive <= floor(500/creep.body.length)
+    if (creep.ticksToLive <= floor(500/creep.body.length))
     {
         console.log(creep.name + 'was renewed.')
         return storage.renewCreep(creep);
