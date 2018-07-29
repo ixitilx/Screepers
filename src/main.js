89ef66d94e5b6046d2cf9dad8c9ad35f67bc7da8
bis <igor.korzhanov@gmail.com>
2018-07-29 22:22:00 +0300

    added source_manager

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
+source_manager = require('source_manager')
+
 exports.loop = function() {
+    _(Game.rooms).map(r => r.find(FIND_SOURCE))
+                 .flatten()
+                 .each(s => source_manager.analyze(s))
     console.log(Game.time)
 }
---
+++ src/source_manager.js
@@ -0,0 +1,48 @@
+function analyze(source) {
+    analyze_findHarvesterPositions(source)
+    analyze_findContainerPosition(source)
+    analyze_releaseUselessHarvesters(source)
+    analyze_requestHarvesters(source)
+    analyze_requestContainer(source)
+    analyze_requestRoad(source)
+}
+
+function analyze_findHarvesterPositions(source) {
+    var pos = source.pos;
+    var terra = source.room.lookForAtArea(
+        LOOK_TERRAIN,
+        pos.y-1,
+        pos.x-1,
+        pos.y+1,
+        pos.x+1,
+        true)
+    console.log('terra:')
+    console.log(JSON.stringify(terra))
+}
+
+function analyze_findContainerPosition(source) {
+
+}
+
+function analyze_releaseUselessHarvesters(source) {
+
+}
+
+function analyze_requestHarvesters(source) {
+
+}
+
+function analyze_requestContainer(source) {
+
+}
+
+function analyze_requestRoad(source) {
+
+}
+
+function operate(source) {
+
+}
+
+exports.analyze = analyze
+exports.operate = operate
