1684bbe9d0e0c977ef8e0da74145b60ae8c6a7b9
bis <igor.korzhanov@gmail.com>
2018-07-29 22:53:36 +0300

    'added debug print'

--- src/source_manager.js
+++ src/source_manager.js
@@ -16,6 +16,7 @@ function analyze_findHarvesterPositions(source) {
     var spots = _(terra).filter(t => t.terrain != 'wall')
                         .map(t => ({'x': t.x, 'y': t.y}))
                         .value()
+    _.each(spots, s => console.log(JSON.stringify(s)))
     _.each(spots, s => room.visual.circle(s.x, s.y))
 
     return spots
