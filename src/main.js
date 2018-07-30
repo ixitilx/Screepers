7474ded7b4dbca1b3d6c2c99fbd0fa17f132fb0b
bis <igor.korzhanov@gmail.com>
2018-07-30 19:46:08 +0300

    'Added global variable'

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 source_manager = require('source_manager')
 
+var sources;
+
 exports.loop = function() {
+    if(sources) {
+        console.log('I have sources: ' + sources)
+    } else {
+        console.log('I do not have sources. Creating some.')
+        sources = _(Game.rooms).first().find(FIND_SOURCES)
+    }
     _(Game.rooms).map(r => r.find(FIND_SOURCES))
                  .flatten()
                  .each(s => source_manager.analyze(s))
