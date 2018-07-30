9db0edb0d89c1e72d461fb0bddef3a164fbc2bcb
bis <igor.korzhanov@gmail.com>
2018-07-30 22:52:28 +0300

    'Fixed code'

--- src/source_manager.js
+++ src/source_manager.js
@@ -10,7 +10,7 @@ function analyze(source) {
 function analyze_findHarvesterPositions(source) {
     const pos = findHarvesterPositions(source);
     console.log(pos);
-    _.each(pos, p => source.room.visual.circle(p, '#FFFF00'));
+    _.each(pos, p => source.room.visual.circle(p, {fill:'#FFFF00'}));
 }
 
 function getHarvesterPositions(source) {
