362f66fb527c564947f4628c1ba12345fa360905
bis <igor.korzhanov@gmail.com>
2018-08-24 18:35:18 +0300

    'Debug logging'

--- src/main.js
+++ src/main.js
@@ -11,7 +11,8 @@ exports.loop = function() {
                  .value();
 
     const t = Task.makeTask('task.dungeon.run', 'init', {});
-    console.log('t.run:', t.run());
+    console.log('t.run:');
+    console.log(t.run());
 
     console.log(Game.time);
 };
