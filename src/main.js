35a16674b7d670ee458f23f9f923be96d3697651
bis <igor.korzhanov@gmail.com>
2018-08-25 18:48:28 +0300

    'Debug logging'

--- src/main.js
+++ src/main.js
@@ -4,13 +4,14 @@ require('task.dungeon.run');
 const Task = require('Task');
 const source_manager = require('source_manager');
 
+const t = Task.makeTask('task.dungeon.run', 'init', {});
+
 exports.loop = function() {
     _(Game.rooms).map(r => r.find(FIND_SOURCES))
                  .flatten()
                  .each(s => source_manager.analyze(s))
                  .value();
 
-    const t = Task.makeTask('task.dungeon.run', 'init', {});
     t.run();
 
     console.log(Game.time);
