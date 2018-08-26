9abc8d49c8d17371a9e51597bc646e4e0f629660
bis <igor.korzhanov@gmail.com>
2018-08-26 19:05:37 +0300

    'wip'

--- src/main.js
+++ src/main.js
@@ -1,41 +1,82 @@
 'use strict';
 
-Object.defineProperty(Room.prototype, 'sources', {
-    get: function() {
-        return this.find(FIND_SOURCES);
-    },
-    enumerable: false,
-    configurable: true,
-});
+function defineProperty(class, name, impl, enumerable=false, configurable=true) {
+    const descriptor =     {
+        get: impl,
+        enumerable: enumerable,
+        configurable: configurable,
+    };
 
-Object.defineProperty(Source.prototype, 'spots', {
-    get: function() {
-        const pos = this.pos;
-        const room = this.room;
-        const terra = room.lookForAtArea(
-                        LOOK_TERRAIN, pos.y-1, pos.x-1, pos.y+1, pos.x+1, true);
-        return _(terra).filter(t => t.terrain === 'plain' || t.terrain === 'swamp')
-                       .map(t => new RoomPosition(t.x, t.y, room.name))
-                       .value();
-    },
-    enumerable: false,
-    configurable: true,
-});
+    Object.defineProperty(class.prototype, name, descriptor);
+};
 
-Object.defineProperty(Source.prototype, 'memory', {
-    get: function() {
-        return _.get(Memory, `sources.${this.id}`);
-    },
-    enumerable: false,
-    configurable: true,
-});
+// -------------
+
+function findSources() {
+    return this.find(FIND_SOURCES);
+};
+
+defineProperty(Room, 'sources', findSources);
+
+// -------------
+
+function findSpots() {
+    const pos = this.pos;
+    const room = this.room;
+    const terra = room.lookForAtArea(
+                    LOOK_TERRAIN, pos.y-1, pos.x-1, pos.y+1, pos.x+1, true);
+    return _(terra).filter(t => t.terrain === 'plain' || t.terrain === 'swamp')
+                   .map(t => new RoomPosition(t.x, t.y, room.name))
+                   .value();
+};
+
+function getSourceMemory() {
+    return _.get(Memory, `sources.${this.id}`, {});
+};
+
+function getHarvesters() {
+    return _.filter(Game.creeps, c => c.memory.source === this.id);
+};
+
+defineProperty(Source, 'memory', findSpots);
+defineProperty(Source, 'spots', getSourceMemory);
+defineProperty(Source, 'harvesters', getHarvesters);
+
+// ------------
 
 function drawSpots(source) {
     _.each(source.spots, pos => Game.rooms[p.roomName].visual.circle(p, {fill:'Yellow'}));
 };
 
+function buildHarvester(source) {
+    const spawn = Game.spawns.Spawn1;
+    const body = [WORK, WORK, CARRY, MOVE];
+    const name = `harvester_${Game.time % 1500}`;
+    const memory = {source: source.id};
+    return spawn.spawnCreep(body, name, {memory:memory});
+};
+
+function runHarvester(creep, spot) {
+    console.log(`Running harvester ${creep} to ${spot}`);
+};
+
+function runHarvesters(source) {
+    const harvoSpots = _.zip(source.spots, source.harvesters);
+    _(harvoSpots).filter(hs => hs[0] && hs[1])
+                 .each(hs => runHarvester(hs[1], hs[0]))
+                 .value();
+};
+
+function buildHarvesters(source) {
+    if (_.size(source.harvesters) < _.size(source.spots)) {
+        buildHarvester(source);
+    }
+};
+
 function harvestSource(source) {
     drawSpots(source);
+    runHarvesters(source);
+    buildHarvesters(source);
 };
 
 exports.loop = function() {
