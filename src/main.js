63a9ede0ddaa621268ea883c18c55031cbb4b61c
bis <igor.korzhanov@gmail.com>
2018-08-27 22:10:02 +0300

    'wip'

--- src/main.js
+++ src/main.js
@@ -18,7 +18,9 @@ function findSources() {
 
 function findEnergy(type=RESOURCE_ENERGY) {
     const droppedEnergy = this.find(FIND_DROPPED_RESOURCES, {resourceType:type});
+    const spawns = _.filter(Game.spawns, {room.name: this.name});
     _.each(droppedEnergy, e => console.log(e.pos, e.amount));
+    _.each(spawns, s => console.log(s.pos, s.energy - s.energyCapacity));
 };
 
 defineProperty(Room, 'sources', findSources);
