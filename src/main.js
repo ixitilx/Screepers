7f02d4c0bfbbc30ab3a75637b002f3ba28321be2
bis <igor.korzhanov@gmail.com>
2018-08-29 21:40:46 +0300

    'Added collectEnergyData()'

--- src/main.js
+++ src/main.js
@@ -9,7 +9,15 @@ require('prototype.creep');
 
 const SourceManager = require('manager.source');
 
+function collectEnergyData() {
+    const spawns = Game.spawns;
+    const haulers = _.filter(Game.creeps, c => _.startsWith(c.name, 'hauler'));
+    const energy = _(Game.rooms).map(r => r.find(FIND_DROPPED_RESOURCES)).flatten().value();
+    console.log(energy);
+};
+
 exports.loop = function() {
     _.each(Game.spawns, s => s.drawSpots());
     _(Game.rooms).map(r => r.sources).flatten().each(SourceManager).value();
+    collectEnergyData();
 };
