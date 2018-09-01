19f001454f3f6c572790f43dd79de524409769c4
bis <igor.korzhanov@gmail.com>
2018-09-01 17:51:20 +0300

    'Added more logic to harvester'

--- src/prototype.creep.js
+++ src/prototype.creep.js
@@ -52,7 +52,7 @@ class Helper {
         if (pos instanceof RoomPosition) {
             if (!pos.isNearTo(target.pos))
                 throw new Error(`Attempt ${this.creep} to harvest ${target} from ${pos}`);
-            if (!pos.isEqual(this.creep.pos))
+            if (!pos.isEqualTo(this.creep.pos))
                 return this.move(pos);
         }
 
