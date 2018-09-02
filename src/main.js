be9f443ce430e5e442462ae1fa6a3c547505acfa
bis <igor.korzhanov@gmail.com>
2018-09-02 14:54:46 +0300

    'CPU profiling'

--- src/main.js
+++ src/main.js
@@ -10,59 +10,7 @@ require('prototype.resource');
 
 const SourceManager = require('manager.source');
 
-function collectEnergyData() {
-    const spawns = _.map(Game.spawns);
-    const haulers = _.filter(Game.creeps, c => _.startsWith(c.name, 'hauler'));
-    const energy = _(Game.rooms).map(r => r.find(FIND_DROPPED_RESOURCES))
-                                .flatten()
-                                .filter(r => r.resourceType === 'energy')
-                                .value();
-
-    const sinks = _([spawns]).flatten()
-                             .filter(s => s.energyLevel < 0)
-                             .sortBy(s => -s.energyLevel)
-                             .value();
-
-    const sources = _([energy]).flatten()
-                               .filter(s => s.energyLevel > 0)
-                               .sortBy(s => -s.energyLevel)
-                               .value();
-
-    return {sinks, sources, haulers};
-};
-
-function buildHauler() {
-    const spawn = Game.spawns.Spawn1;
-    const body = [CARRY, CARRY, CARRY, CARRY, MOVE, MOVE];
-    const name = `hauler_${Game.time % 1500}`;
-    return spawn.spawnCreep(body, name);
-};
-
-function routeEnergy(sinks, sources, haulers) {
-    if (_.size(sinks) === 0 || _.size(sources) === 0) {
-        console.log(`No sinks or sources. Sinks: ${sinks}, Sources: ${sources}`);
-        return;
-    }
-
-    if (_.size(haulers) === 0) {
-        const ret = buildHauler();
-        console.log('Building hauler!', ret);
-        return;
-    }
-
-    for(const i=0; i<sinks.length; i++) {
-        console.log(sinks[i]);
-    }
-
-    // console.log(`sinks: ${sinks}`);
-    // console.log(`sources: ${sources}`);
-    // console.log(`haulers: ${haulers}`);
-};
-
 exports.loop = function() {
     _.each(Game.spawns, s => s.drawSpots());
     _(Game.rooms).map(r => r.sources).flatten().each(SourceManager).value();
- 
-    // const {sinks, sources, haulers} = collectEnergyData();
-    // routeEnergy(sinks, sources, haulers);
 };
