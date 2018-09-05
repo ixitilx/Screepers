6a33d843b30df8f9ca0acda204ed1dec6c0654a8
bis <igor.korzhanov@gmail.com>
2018-09-05 21:47:22 +0300

    'drawsomething'

--- src/prototype.room.js
+++ src/prototype.room.js
@@ -6,13 +6,13 @@ function findSources() {
     return this.find(FIND_SOURCES);
 };
 
-const terrainMap = {
+const defaultTerrainMap = {
     'wall' : '#',
     'plain': ' ',
     'swamp': '*',
 };
 
-function scanTerrain() {
+function scanTerrain(terrainMap=defaultTerrainMap) {
     const room = this;
     return Array.from({length: 50}, (v, i) => {
         return room.lookForAtArea(LOOK_TERRAIN, i, 0, i, 49, true)
