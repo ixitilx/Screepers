2b3f430be737fbbc942d6e894835b62e9e13ba91
bis <igor.korzhanov@gmail.com>
2018-09-06 00:23:07 +0300

    'drawsomething'

--- src/prototype.room.plan.terrain.js
+++ src/prototype.room.plan.terrain.js
@@ -127,13 +127,13 @@ function drawSomething(room) {
 
     const sourcePos = _.map(room.find(FIND_SOURCES), 'pos');
     const distanceMap = buildDistanceMap(sourcePos, terrainMap).normalize();
+    distanceMap.data.forEach(row => console.log(row));
     cpy = Game.cpu.getUsed();
     console.log('distanceMap', cpy-cpu);
     cpu = cpy;
 
     const colorMap = buildColorMap(distanceMap);
     cpy = Game.cpu.getUsed();
-    colorMap.forEach(row => console.log(row));
     console.log('colorMap', cpy-cpu);
     cpu = cpy;
 
