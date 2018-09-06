a3276a577975c9ef11d13001dc9a18afb87b72cd
bis <igor.korzhanov@gmail.com>
2018-09-06 23:56:43 +0300

    'ScoreMap'

--- src/prototype.room.plan.terrain.js
+++ src/prototype.room.plan.terrain.js
@@ -207,7 +207,7 @@ function drawSomething(room) {
         scoreMap => scoreMap.inverse().normalize());
 
     const exitMaps = getExits(terrainMap);
-    exitMaps.forEach(m => console.log(m));
+    exitMaps.forEach(m => console.log(JSON.stringify(m)));
 
     // const exitMap = ScoreMap.combine(_.sum, exitMaps).normalize();
 
