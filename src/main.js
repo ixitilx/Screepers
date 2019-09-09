8bd4ed342fa18178307279fb01c6aea87545d848
bis <igor.korzhanov@gmail.com>
2019-09-10 00:15:35 +0300

    Reset back to v0.0

--- src/main.js
+++ src/main.js
@@ -1,15 +1,4 @@
 'use strict';
 
-require('prototype.source');
-const sourceManager = require('manager.source.2');
-
-const ident = require('ident');
-console.log(`Reinitialized at ${Game.time}. Hello ${ident.me}!`);
-
 exports.loop = function() {
-    _(Game.rooms)
-        .map(room => room.find(FIND_SOURCES))
-        .flatten()
-        .each(source => sourceManager(source))
-        .value();
 };
