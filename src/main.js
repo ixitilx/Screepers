320217e59cc6ee190dc28896f304387aaeaed44a
bis <igor.korzhanov@gmail.com>
2019-10-05 16:55:52 +0300

    no message

--- src/main.js
+++ src/main.js
@@ -7,8 +7,10 @@ exports.loop = function() {
     // const source = room.find(FIND_SOURCES)[0];
     // sourceHarvest(source);
 
-    const spawn = _(Game.spawns).first();
-    console.log(spawn);
-
-    // const creepCountBefore = _.size(Game.creeps);
+    const spawn = Game.spawns.Spawn1;
+    const creepCountBefore = _.size(Game.creeps);
+    if (spawn.spawnCreep([MOVE], `move_${Game.time}`) === OK) {
+        const creepCountAfter = _.size(Game.creeps);
+        console.log(`Tick: ${Game.time}. CreepCount ${creepCountBefore} -> ${creepCountAfter}`);
+    }
 };
